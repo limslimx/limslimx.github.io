b64d1bb0de05395b023ee0f8b9ccc3461925f827
saemyunglim <saemyung2000@naver.com>
2020-04-11 17:40:58 +0900

    _config.yml 파일 수정

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,11 +3,11 @@ var store = [{
         "excerpt":"기본적인 텍스트 표기 방식이다. 마크다운은 줄바꿈을 인식하지 않는다. 줄바꿈을 하기 위해서는 라인 끝에 스페이스를 2번 표기해야 한다. 여러가지 강조 표시가 존재한다. 첫번째로 single asterisks, 두번째로 single underscores, 세번째로 double asterisks, 네번째로 double underscores, 다섯번째로 cancelline이 있다. 1. 앞에 #을 붙이는 개수에 따라 html의 H1~H6 태그처럼 글자크기가 바뀐다. # This is...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/Markdown%EC%97%90-%EB%8C%80%ED%95%98%EC%97%AC-%EC%95%8C%EC%95%84%EB%B3%B4%EA%B8%B0/",
-        "teaser": null
+        "teaser": "http://localhost:4000/assets/images/football.jpeg"
       },{
         "title": "github 블로그 시작하기",
         "excerpt":"Github Blog 서비스인 github.io를 이용하여 IT 개발 블로그를 개설하기로 했다.   에서 정의한 변수를 이중 괄호 구문으로 본문에 추가할 수 있다. 이 글의 제목은 github 블로그 시작하기이고 수정 시간은 2020-04-11 16:00:00 +0000이다.   ","categories": ["Blog"],
         "tags": ["Blog"],
         "url": "http://localhost:4000/blog/first-post/",
-        "teaser": null
+        "teaser": "http://localhost:4000/assets/images/football.jpeg"
       }]
